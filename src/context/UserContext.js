2b50d4708116f8c3c89c995dcf4c7db7cae24000
deepaksh798 <deepak.sh798@gmail.com>
2024-03-20 22:03:57 +0530

    use useContext and create login page

---
+++ src/context/UserContext.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const UserContext = React.createContext();
+
+export default UserContext;
+
+//Here we create a Context and store in a variable UserContext and then export
+
+//here we use .js file because there is no need to .jsx file
